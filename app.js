d876c42faced168ce791590f906b99a6e345498a
unknown <dewesh1997@gmail.com>
2020-05-18 07:35:15 +0530

    Added env variables

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -30,8 +31,8 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-const secret = "Thisisoursecreetwhcihislittle";
-userSchema.plugin(encrypt, {secret:secret, encryptedFields: ["password"]});
+
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = mongoose.model("User", userSchema);
